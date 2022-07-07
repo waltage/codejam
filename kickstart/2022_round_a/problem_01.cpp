0ee58ebeebb44c7ab2c6d4d804fcf1336949f35d
Daniel Walt <daniel.r.walt@gmail.com>
2022-07-07 14:56:31 -0400

    finished problem 1

--- kickstart/2022_round_a/problem_01.cpp
+++ kickstart/2022_round_a/problem_01.cpp
@@ -1,26 +1,55 @@
 #include <iostream>
-
-
-
+#include <string>
 
 
 struct TestCase {
-    int i = 0;
+    std::string expected;
+    std::string input;
 
     TestCase() {  // NOLINT
-    // Parse Case.
-
-    std::cin >> i;
-
+      std::cin >> expected >> input;
     };
 };
 
 
-int solve(TestCase& c) {
+std::string solve(TestCase& c) {
+  if (c.input.size() < c.expected.size()) {
+    // input not long enough
+    return "IMPOSSIBLE";
+  }
 
+  if (c.input.size() == c.expected.size()) {
+    if (c.input == c.expected) {
+      // same string
+      return "0";
+    } else {
+      // otherwise, can't remove any
+      return "IMPOSSIBLE";
+    }
+  }
 
-
-  return 0;
+  size_t exp_ptr = c.expected.size() - 1;
+  size_t inp_ptr = c.input.size() - 1;
+  int drop_cnt = 0;
+  while (exp_ptr > 0 && inp_ptr > 0) {
+    // until we run out of characters, move right to left
+    if (c.expected[exp_ptr] == c.input[inp_ptr]) {
+      // match, dec both
+      exp_ptr--;
+      inp_ptr--;
+    } else {
+      // no match, need to drop from input
+      drop_cnt++;
+      inp_ptr--;
+    }
+  }
+  if (exp_ptr > 0) {
+    // did not reach the end of the expected string
+    return "IMPOSSIBLE";
+  }
+  // we've hit the end of the expected string, so any remaining
+  // inputs must be dropped as well...
+  return std::to_string(drop_cnt + inp_ptr);
 }
 
 
