0354c38f690373514a3d8611d3c53ae06b12aa90
Daniel Walt <daniel.r.walt@gmail.com>
2022-07-07 14:28:36 -0400

    setup kickstart 2022 round a

---
+++ kickstart/2022_round_a/problem_02.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+
+
+
+
+struct TestCase {
+    int i = 0;
+
+    TestCase() {  // NOLINT
+    // Parse Case.
+
+    std::cin >> i;
+
+    };
+};
+
+
+int solve(TestCase& c) {
+
+
+
+  return 0;
+}
+
+
+
+// ===================================================================
+void run_all();
+
+int main() {
+	run_all();
+	return 0;
+}
+
+void run_all() {
+	int t;
+	std::cin >> t;
+	for (int cid = 1; cid < t + 1; cid++) {
+		TestCase c;
+		auto result = solve(c);
+		std::cout << "Case #" << cid << ": " << result << std::endl;
+	}
+}
