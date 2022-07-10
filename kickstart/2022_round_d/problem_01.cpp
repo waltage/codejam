225edc73ce0089404da390a8a89a7ef42d81e15d
Daniel Walt <daniel.r.walt@gmail.com>
2022-07-10 11:41:28 -0400

    submission for problem 1; will add real stdin/out afterward

--- kickstart/2022_round_d/problem_01.cpp
+++ kickstart/2022_round_d/problem_01.cpp
@@ -1,30 +1,50 @@
 /*
- * Copyright 2022 _____ 
+ * Copyright 2022 Daniel Walt
  */
-
 #include <iostream>
-
-
+#include <vector>
+#include <algorithm>
 
 
 
 struct TestCase {
-  int i = 0;
+  int regions = 0;
+  int categories = 0;
+  std::vector<uint32_t> participants;
 
   TestCase() {  // NOLINT
-  // Parse Case.
-
-  std::cin >> i;
-
+    std::cin >> regions >> categories;
+    participants.resize(regions);
+    for (int i = 0; i < regions; i++) {
+      std::cin >> participants[i];
+    }
   }
 };
 
 
-int solve(TestCase* c) {
+double solve(TestCase* c) {
+  // with sorted participants
+  // last M - 1 regions all as themselves,
+  // first N-M in a single group
 
+  std::sort(c->participants.begin(), c->participants.end());
+  double agg = 0;
+  int ptr = static_cast<int>(c->participants.size() - 1);
+  for (int i = c->categories; i > 1; i--) {
+    agg += c->participants[ptr];
+    ptr--;
+  }
+  if (ptr % 2 == 0) {
+    // odd number
+    agg += c->participants[ptr/2];
+  } else {
+    // even, split
+    double p1 = c->participants[ptr/2];
+    double p2 = c->participants[ptr/2 + 1];
+    agg += (p1 + p2) / 2;
+  }
 
-
-  return 0;
+  return agg;
 }
 
 
@@ -33,6 +53,7 @@ int solve(TestCase* c) {
 void run_all();
 
 int main() {
+  std::cout.precision(7);
   run_all();
   return 0;
 }
@@ -43,6 +64,6 @@ void run_all() {
   for (int cid = 1; cid < t + 1; cid++) {
     TestCase c;
     auto result = solve(&c);
-    std::cout << "Case #" << cid << ": " << result << std::endl;
+    std::cout << "Case #" << cid << ": " << std::fixed << result << std::endl;
   }
 }
